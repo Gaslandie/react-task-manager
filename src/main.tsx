96945bd042454b01fa80cc1920c2a63c82c94027
Mohamed Gassama <gassma803@gmail.com>
2025-12-29 14:37:58 +0000

    nettoyage des fichiers par défaut de vite et branchement du AppRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import './assets/styles/main.scss'
+import AppRouter from './routes/AppRouter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 )
